193754d4761dac1b8468d523a43eeb19e5c22a0e
Fadil Abdussemee <116811583+fierychicken65@users.noreply.github.com>
2025-03-13 14:20:11 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkin\n";
+  cout << "Hello, Jenk\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
